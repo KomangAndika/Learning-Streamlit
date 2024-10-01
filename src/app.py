415fd382ec38542721dd29fd393c65cb5e6f3f8a
KomangAndika <kwira.santosa@gmail.com>
2024-10-01 13:56:23 +0700

    adding-csv

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
 import streamlit as st # type: ignore
+import pandas as pd
+
 st.write("Hellow world")
 x = st.text_input("Favorite Food?")
-st.write(f"Your fav food is:{x}")
+st.write(f"Your fav food is: {x}")
