7e81295ca702ba6c02d252800f594f573a46bdc5
KomangAndika <kwira.santosa@gmail.com>
2024-10-01 17:00:08 +0700

    fixing-bmi-logic

--- src/BMI-Calc.py
+++ src/BMI-Calc.py
@@ -6,7 +6,7 @@ st.title("This is a BMI Calc")
 st.write("Calc stands for calculator for anyone wondering")
 
 height = st.number_input("Enter your height (in meter): ",min_value=0.5,max_value=2.5,step=0.01)
-weight = st.number_input("Enter your weight (in kilos): ",min_value=10,max_value=100,step=1)
+weight = st.number_input("Enter your weight (in kilos): ",min_value=10,max_value=500,step=1)
 happy = st.slider("How happy you are from 1 to 10",min_value=1,max_value=10,step=1)
 
 def calculate_bmi(weight, height):
@@ -23,9 +23,9 @@ if st.button("Calc BMI"):
     elif res < 18.5 and happy > 5:
         st.write("you are skinny and happy")
 
-    elif (res < 18.5 and res > 24.9) and happy < 5:
+    elif (res >= 18.5 and res <= 24.9) and happy < 5:
         st.write("you are ok and sad as fuck")
-    elif (res < 18.5 and res > 24.9) and happy > 5:
+    elif (res >= 18.5 and res <= 24.9) and happy > 5:
         st.write("you are ok and happy")
 
     elif res > 24.9 and happy > 5:
