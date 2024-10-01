333e7e86515055e1f15cedbe5b953c3971991c58
KomangAndika <kwira.santosa@gmail.com>
2024-10-01 14:55:09 +0700

    creating-notes

---
+++ src/notes.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+# st.write commands
+text = "helluva"
+st.write("Hello my broski")
+st.write(text)
+
+# Anytime streamlit see a variabel or literal value on its own line, automatically writes that as a st.write
+text
+"skibidi toilet"
