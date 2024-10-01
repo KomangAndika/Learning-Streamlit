5db539a434470d57437ddd5565cdc6f473cc46ee
KomangAndika <kwira.santosa@gmail.com>
2024-10-01 14:19:54 +0700

    yeah

--- src/loading-data.py
+++ src/loading-data.py
@@ -1,5 +1,15 @@
 import streamlit as st # type: ignore
 import pandas as pd # type: ignore
+import numpy as np # type: ignore
 
 data = pd.read_csv("/Users/komangandikawirasantosa/Learning-Streamlit/data/loan_data.csv")
 st.write(data)
+
+st.write("# Cool Chart init?")
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a","b","c"]
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
