3e5d14ed869ecf61ae0beee41737183259ac94ef
KomangAndika <kwira.santosa@gmail.com>
2024-10-02 11:04:45 +0700

    making-some-write-notes

---
+++ src/pages/1_profile.py
@@ -0,0 +1,11 @@
+# This is for learning about how formatted text work
+import streamlit as st
+
+st.title("This is a title eh")
+st.header("This is my header")
+st.header("This is the subheader")
+
+st.caption("This is a simple caption")
+st.code("x = 6969 this is a code block btw")
+st.divider()
+st.text("Hello world")
