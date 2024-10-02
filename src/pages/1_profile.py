1cf79c9b56e952eb46f01400da37af4a2ac6b98a
KomangAndika <kwira.santosa@gmail.com>
2024-10-02 14:26:14 +0700

    adding-logo

--- src/pages/1_profile.py
+++ src/pages/1_profile.py
@@ -1,7 +1,10 @@
 # This is for learning about how formatted text work
 import streamlit as st
 
+
 st.title("This is a title eh")
+
+st.logo("/Users/komangandikawirasantosa/Learning-Streamlit/data/noun-bike-7257848.png")
 st.header("This is my header")
 st.header("This is the subheader")
 
